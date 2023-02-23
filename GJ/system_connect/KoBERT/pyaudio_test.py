a5068e615b839f314e9cf320a5e063c2f353dff4
KwanJung98 <remf123@gmail.com>
2023-02-23 21:54:40 +0900

    Add sample sexual dictionary

--- GJ/system_connect/KoBERT/pyaudio_test.py
+++ GJ/system_connect/KoBERT/pyaudio_test.py
@@ -13,13 +13,17 @@ import librosa
 import numpy as np
 import keyboard
 
+from kiwipiepy import Kiwi
+import make_dict
+
+
 API_BASE = "https://openapi.vito.ai"
 
-# 초당 16000개의 frame을 3200 frame 단위로 나눠서 전달
-CHUNK = 3200
+# 초당 16000개의 frame을 3200 frame 단위로 나눠서 전달 --> DEFAULT_BUFFER_SIZE로 수정
+# CHUNK = 3200
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
-RATE = 16000
+RATE = 44100
 
 class VITOOpenAPIClient:
     def __init__(self, client_id, client_secret):
@@ -29,6 +33,9 @@ class VITOOpenAPIClient:
         self.client_secret = client_secret
         self._sess = Session()
         self._token = None
+
+        self.sexual_dict = make_dict.Sexual_dict()
+        self.kiwi = Kiwi()
 
         self.amplitude = 0
         self.tempo = 0
@@ -51,7 +58,7 @@ class VITOOpenAPIClient:
     async def streaming_transcribe(self, audio_generator, config=None):
         if config is None:
             config = dict(
-                sample_rate="16000",
+                sample_rate="44100",
                 encoding="LINEAR16",
                 use_itn="true",
                 use_disfluency_filter="false",
@@ -92,7 +99,6 @@ class VITOOpenAPIClient:
         async def transcriber(websocket):
             async for msg in websocket:
                 msg = json.loads(msg)
-                #print(msg)
                 if msg["final"]:
                     text = msg["alternatives"][0]["text"]
                     idx = msg["seq"]
@@ -105,7 +111,7 @@ class VITOOpenAPIClient:
                     speech_tempo = len(msg["alternatives"][0]["text"]) * 1000 / msg["duration"]
                     self.tempo = (self.tempo + speech_tempo) / cnt
 
-                    speech_amplitude = await analyzer(msg)
+                    speech_amplitude = analyzer(msg)
                     self.amplitude = (self.amplitude * (cnt-1) + speech_amplitude) / cnt
 
                     print("발화 속도 : ", speech_tempo)
@@ -113,24 +119,33 @@ class VITOOpenAPIClient:
                     print("발화 크기: ", speech_amplitude)
                     print("평균 발화 크기: ", self.amplitude)
 
-                    await bert(idx)
+                    # 문장 형태소 분리
+                    morphs = self.kiwi.tokenize(text)
+                    match_flag = False
+                    for x in morphs:
+                        if self.sexual_dict.match(x.form):
+                            print(x,"가 성희롱 사전과 매칭되었습니다.")
+                            match_flag = True
 
-        async def analyzer(msg):
-            edited_data = await split_wav(16000, msg["start_at"], msg["start_at"]+msg["duration"])
+                    if match_flag == True:     
+                        bert(idx)
+
+        def analyzer(msg):
+            edited_data = split_wav(44100, msg["start_at"], msg["start_at"]+msg["duration"])
             stft = librosa.stft(edited_data, n_fft = 1600, hop_length = 400)
             spectrogram = np.abs(stft)
             log_spectrogram = librosa.amplitude_to_db(spectrogram)
     
             return log_spectrogram.mean(axis=0).mean(axis=0)
 
-        async def split_wav(sample_rate, start, end):
+        def split_wav(sample_rate, start, end):
             start *= sample_rate
             end *= sample_rate
             start = int(start /1000)
             end = int(end / 1000)
             return self.amplitude_list[start:end]
             
-        async def bert(idx):
+        def bert(idx):
             print("=====BERT 진입======")
             print("["+texts[idx][1] + "]을 ai 분류기에 투입")
             self.model.predict(texts[idx][1])                
@@ -148,8 +163,8 @@ if __name__ == "__main__":
     client = VITOOpenAPIClient(CLIENT_ID, CLIENT_SECRET)
 
     p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
-    audio_generator = iter(lambda: stream.read(CHUNK), b"")
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=4096)
+    audio_generator = iter(lambda: stream.read(DEFAULT_BUFFER_SIZE), b"")
     asyncio.run(client.streaming_transcribe(audio_generator))
     stream.stop_stream()
     stream.close()
