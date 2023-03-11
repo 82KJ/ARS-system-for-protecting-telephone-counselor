c4f4a9370b1e4abb38f9ec7cdc7a4493480c3578
KwanJung98 <remf123@gmail.com>
2023-03-11 18:01:27 +0900

    Add immoral sentence data (2-set, 500/5000)

--- GJ/system_connect/KoBERT/tkinter_test.py
+++ GJ/system_connect/KoBERT/tkinter_test.py
@@ -1,76 +1,135 @@
 import tkinter as tk
 import pyaudio
-import wave
-import requests
+import websockets
+import asyncio
+import numpy as np
+import logging
+from requests import Session
+import time
 import json
-import threading
 
-# GUI 생성
-root = tk.Tk()
-root.geometry('300x100')
+API_BASE = "https://openapi.vito.ai"
 
-# 버튼 클릭 시 실행되는 함수
-def start_recording():
-    # 녹음 설정
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 44100
-    RECORD_SECONDS = 5
-    WAVE_OUTPUT_FILENAME = "recorded.wav"
+
+# 웹소켓 서버 URL 설정
+WEBSOCKET_SERVER_URL = "ws://localhost:8000"
+
+# Pyaudio 설정
+CHUNK_SIZE = 1024
+SAMPLE_RATE = 44100
+RECORD_SECONDS = 5
+CHANNELS = 1
+FORMAT = pyaudio.paInt16
+
+class App:
+    def __init__(self, master, client_id, client_secret):
+        super().__init__()
+        self._logger = logging.getLogger(__name__)
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self._sess = Session()
+        self._token = None
+
+        self.master = master
+        self.is_recording = False
+        
+        # 녹음 버튼 생성
+        self.record_button = tk.Button(master, text="Record", command=self.start_recording)
+        self.record_button.pack()
+        
+        # 녹음 상태 표시 라벨 생성
+        self.status_label = tk.Label(master, text="Not recording")
+        self.status_label.pack()
+        
+        # 결과 표시 라벨 생성
+        self.result_label = tk.Label(master, text="결과창")
+        self.result_label.pack()
+        
+        # 웹소켓 연결
+        self.ws = None
+        
+        # Pyaudio 초기화
+        self.audio = pyaudio.PyAudio()
+        self.stream = self.audio.open(format=FORMAT,
+                                      channels=CHANNELS,
+                                      rate=SAMPLE_RATE,
+                                      input=True,
+                                      frames_per_buffer=CHUNK_SIZE)
+
+
+
+    @property
+    def token(self):
+        if self._token is None or self._token["expire_at"] < time.time():
+            resp = self._sess.post(
+                API_BASE + "/v1/authenticate",
+                data={"client_id": self.client_id, "client_secret": self.client_secret},
+            )
+            resp.raise_for_status()
+            self._token = resp.json()
+        return self._token["access_token"]
+
+
+    async def send_stream(self, websocket):
+        while self.is_recording:
+            data = self.stream.read(CHUNK_SIZE)
+            await websocket.send(data)
+        
+    async def receive_result(self, websocket):
+        while self.is_recording:
+            result = await websocket.recv()
+            msg = json.loads(result)
+            #print(msg)
+            if msg["final"] :
+                print(msg["alternatives"][0]["text"])   
+            # 결과를 실시간으로 표시
+                self.result_label.config(text=msg["alternatives"][0]["text"])
+        
+    async def start_websocket(self, config=None):
+        if config is None:
+            config = dict(
+                sample_rate="44100",
+                encoding="LINEAR16",
+                use_itn="true",
+                use_disfluency_filter="false",
+                use_profanity_filter="false",
+            )
+
+        STREAMING_ENDPOINT = "wss://{}/v1/transcribe:streaming?{}".format(
+            API_BASE.split("://")[1], "&".join(map("=".join, config.items()))
+        )
+        conn_kwargs = dict(extra_headers={"Authorization": "bearer " + self.token})
+
+        async with websockets.connect(STREAMING_ENDPOINT, **conn_kwargs) as websocket:
+            self.ws = websocket
+            await asyncio.gather(
+                self.send_stream(websocket), 
+                self.receive_result(websocket)
+            )
+
+    def update_result_label(self):
+        # 결과 표시 라벨 업데이트
+        if self.is_recording:
+            result = self.result_label["text"] + "."
+            self.result_label.config(text=result)
+        # 일정 시간 간격으로 업데이트 호출
+        self.master.after(500, self.update_result_label)
     
-    # 녹음 스레드 생성 및 시작
-    def record():
-        p = pyaudio.PyAudio()
-        stream = p.open(format=FORMAT,
-                        channels=CHANNELS,
-                        rate=RATE,
-                        input=True,
-                        frames_per_buffer=CHUNK)
+    def start_recording(self):
+        self.is_recording = True
+        self.status_label.config(text="Recording...")
+        
+        asyncio.run(self.start_websocket())
+        # 녹음 종료
+        self.status_label.config(text="Not recording")
+        self.stream.stop_stream()
+        self.stream.close()
+        self.audio.terminate()
+    
 
-        frames = []
+CLIENT_ID = "NFJOMvUAYfhQiZA8ZQit"
+CLIENT_SECRET = "XJ2pgsDOaR8RilnUjvHKwaF9_WbXVjwcsfPMHx5L"
 
-        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-            data = stream.read(CHUNK)
-            frames.append(data)
-
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
-
-        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(p.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-        wf.writeframes(b''.join(frames))
-        wf.close()
-
-    # 스트리밍 스레드 생성 및 시작
-    def stream():
-        url = 'https://api.vito.ai/v1/speech-to-text'
-        headers = {
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzgyMDI1MTksImlhdCI6MTY3ODE4MDkxOSwianRpIjoicDRiMWczNFBVak9odTU3d1ZFYkMiLCJwbGFuIjoiYmFzaWMiLCJzY29wZSI6InNwZWVjaCIsInN1YiI6Ik5GSk9NdlVBWWZoUWlaQThaUWl0In0.exOhBgT7FzRKNFaVNwPjaIx2OO0Tk8xS80KfJ0JF54I',
-            'Content-Type': 'audio/wav'
-        }
-        data = open(WAVE_OUTPUT_FILENAME, 'rb').read()
-        response = requests.post(url, headers=headers, data=data)
-        result = json.loads(response.content)
-
-        # 결과를 출력
-        result_label.configure(text=result['result'])
-
-    record_thread = threading.Thread(target=record)
-    stream_thread = threading.Thread(target=stream)
-
-    record_thread.start()
-    stream_thread.start()
-
-# 버튼 생성
-button = tk.Button(root, text='Record', command=start_recording)
-button.pack(pady=20)
-
-# 결과를 출력할 라벨 생성
-result_label = tk.Label(root, text='')
-result_label.pack()
-
+root = tk.Tk()
+app = App(root, CLIENT_ID, CLIENT_SECRET)
 root.mainloop()
