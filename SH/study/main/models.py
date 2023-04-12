7a899e9834f53e6c7387796f91c15bfe3af2d617
Byabya <thditkfkd@gmail.com>
2023-04-12 20:35:41 +0900

    studying django

--- SH/study/main/models.py
+++ SH/study/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Detection(models.Model):
+    #분류 order
+    #욕설 탐지 시각 detect_time
+    #욕설 탐지 분류 result
+    #욕서 탐지 내역 content
+    
+    order = models.CharField(max_length = 200) 
+    detect_date = models.DateTimeField()   
+    result = models.CharField(max_length = 15)   
+    content = models.TextField()
