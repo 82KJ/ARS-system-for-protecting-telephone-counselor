8754bd2d88767ca42cf2aa0a48cfefccd0ca2345
penpenguin2018 <penpenguin2018@gmail.com>
2023-04-30 17:43:03 +0900

    result 수정중

--- Telmet/main/views.py
+++ Telmet/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from main.models import ConversationLog
 
 def home(request):
     return render(request, 'main/home.html')
@@ -7,4 +8,18 @@ def chatroom(request):
     return render(request, 'main/chatroom.html')
 
 def result(request):
-    return render(request, 'main/result.html')
+    data = []
+    conversation_log = ConversationLog.objects.all()
+    count = ConversationLog.objects.count()
+    #objects.count 바꿔야함.
+    abuse_count = ConversationLog.objects.count()
+    sexual_count = ConversationLog.objects.count()
+    for log in conversation_log:
+        data.append({'content': log.content, 'result' : log.result})
+    context = {'data': data, 
+                'count': count, 
+                'abuse_count' : abuse_count,
+                'sexual_count' : sexual_count
+                }
+    return render(request, 'main/result.html', context)
+
