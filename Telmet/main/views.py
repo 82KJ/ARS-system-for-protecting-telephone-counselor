eb1901149f3ac82b442559f7cb94355e36eae062
KwanJung98 <remf123@gmail.com>
2023-05-03 14:52:22 +0900

    ChatRoom 현재 시각 정보 추가 구현

--- Telmet/main/views.py
+++ Telmet/main/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from main.models import ConversationLog
+from datetime import datetime
 
 def home(request):
     return render(request, 'main/home.html')
 
 def chatroom(request):
-    return render(request, 'main/chatroom.html')
+    res = {}
+
+    # 현재 시간 처리 진행
+    now = datetime.now()
+
+    date = str(now.date())
+    weekdays = ['MON', 'TUE', 'WEN', 'THR', 'FRI', 'SAT', 'SUN']
+    day_of_week = weekdays[now.weekday()]
+    time = str(now.time())[:-7]
+
+    cur_time = date + " " + day_of_week + " " + time
+    res['cur_time'] = cur_time
+    
+    return render(request, 'main/chatroom.html', res)
 
 def result(request):
     data = []
